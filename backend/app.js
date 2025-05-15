53fc289def2dcfa3ab3654e62a81f5bb42130f09
jad-png <j.belassiria@uca.ca.ma>
2025-05-15 18:29:23 +0100

    FEAT: set up Express app with JSON parsing and error handling; connect to database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
-
 const userRoutes = require('./routes/userRoutes');
-
 const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use('/api', );
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message || 'Server Error' });
+});
+
+module.exports = app;

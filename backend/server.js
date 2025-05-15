ffb39c2d4eb0c254d8f5d09e3b77ff9e3e417514
jad-png <j.belassiria@uca.ca.ma>
2025-05-15 15:52:41 +0100

    FEAT: configure environment variables and update server port settings

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });

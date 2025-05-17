d7b49855770e5637358ec6111e2a59429c6e4ebc
jad-png <j.belassiria@uca.ca.ma>
2025-05-17 15:56:39 +0100

    feat: implement login function in userController to authenticate users and generate JWT tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,3 +12,21 @@ const register = async (req, res, next) => {
         next(err);
     }
 };
+
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        if (user.isBanned) {
+            return res.status(403).json({ message: 'This user is banned' });
+        }
+        const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token }); 
+    } catch (err) {
+        next(err);
+    }
+};
+

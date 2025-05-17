cf0c10cc35eddacf6cf314f53d34781c32407bf8
jad-png <j.belassiria@uca.ca.ma>
2025-05-17 15:36:03 +0100

    FEAT: update user model to include role, isBanned, and timestamps; rename name to username

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,55 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
   },
-
   email: {
     type: String,
     required: true,
     unique: true,
   },
-
   password: {
     type: String,
     required: true,
   },
-  favorites: [{
+  role: {
+    type: String,
+    enum: ['Client', 'Coach'],
+    required: true,
+    default: 'Client',
+  },
+  isBanned: {
+    type: Boolean,
+    default: false,
+  },
+  favoritesWorkouts: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: "Workout"
   }],
+  enrolledWorkouts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Workout",
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  this.updatedAt = Date.now();
+  next();
+});
+
+
+module.exports = mongoose.model('User', userSchema);
